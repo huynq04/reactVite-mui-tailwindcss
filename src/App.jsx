51fa1b4e24f2ea31c71a166f0c3c6881fd2702cb
huynqbg <nqhuysmbg@gmail.com>
2024-01-23 12:56:37 +0700

    mounted & unmounted

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,15 @@
 import { useState } from 'react';
+import Content from './content';
 import './App.css';
 
 function App() {
-  const [todo, setTodo] = useState('');
+  const [show, setShow] = useState(false);
 
-  let newTodos;
-  let newTodosString;
-
-  // initial state chi chay 1 lan duy nhat khi component duoc render
-  const [todos, setTodos] = useState(() => {
-    const todosString = localStorage.getItem('todos');
-    const arrTodo = JSON.parse(todosString);
-
-    return arrTodo ?? [];
-  });
-
-  const handleAdd = () => {
-    setTodos((prev) => {
-      if (!todo) return prev;
-
-      newTodos = [...prev, todo];
-
-      newTodosString = JSON.stringify(newTodos);
-      localStorage.setItem('todos', newTodosString);
-
-      return newTodos;
-    });
-    setTodo(''); // Reset input
-  };
-
-  const removeTodo = (index) => {
-    // Phải setTodos thì mới render lại giao diện
-    setTodos((prev) => {
-      newTodos = prev.filter((todo, i) => i !== index);
-
-      newTodosString = JSON.stringify(newTodos);
-      localStorage.setItem('todos', newTodosString);
-
-      return newTodos;
-    });
-  };
   return (
-    <>
-      <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-
-      <button onClick={handleAdd} type="submit">
-        Add
-      </button>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            <span style={{ marginRight: 20 }}>{todo}</span>
-            <button onClick={() => removeTodo(index)}>X</button>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="App">
+      <button onClick={() => setShow(!show)}>toggle</button>
+      {show && <Content />}
+    </div>
   );
 }
 
---
+++ src/Content.jsx
@@ -0,0 +1,9 @@
+function Content() {
+  return (
+    <div>
+      <h1>Hello anh em</h1>
+    </div>
+  );
+}
+
+export default Content;
