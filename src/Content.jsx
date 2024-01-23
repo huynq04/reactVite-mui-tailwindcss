778eac0a70cad698a595665f9af4429baad2d0d2
huynqbg <nqhuysmbg@gmail.com>
2024-01-23 17:39:30 +0700

    useEffect with [ ]

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
+
 function Content() {
-  return (
-    <div>
-      <h1>Hello anh em</h1>
-    </div>
-  );
+    const [title, setTitle] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then((response) => response.json())
+            .then((json) => setPosts(json));
+    }, []);
+
+    return (
+        <div>
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+
+            <ul>
+                {posts.map((item, index) => (
+                    <li key={index.id}>{item.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default Content;
