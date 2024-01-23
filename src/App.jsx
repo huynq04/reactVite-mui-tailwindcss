bc95fab7216ad2007a64c51eb1488ab901847da7
huynqbg <nqhuysmbg@gmail.com>
2024-01-23 18:22:45 +0700

    useEffect() with DOM events

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Content from './content';
 import './App.css';
 
 function App() {
-  const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
 
-  return (
-    <div className="App">
-      <button onClick={() => setShow(!show)}>toggle</button>
-      {show && <Content />}
-    </div>
-  );
+    return (
+        <div className="App">
+            <button onClick={() => setShow(!show)}>toggle</button>
+            {show && <Content />}
+        </div>
+    );
 }
 
 export default App;
--- src/Content.jsx
+++ src/Content.jsx
@@ -3,15 +3,28 @@ import { useEffect, useState } from 'react';
 const tabs = ['posts', 'comments', 'albums', 'photos', 'todos', 'users'];
 
 function Content() {
-    const [title, setTitle] = useState('');
     const [posts, setPosts] = useState([]);
     const [activeTap, setActiveTap] = useState(tabs[0]);
+    const [backTop, setBackTop] = useState(false);
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${activeTap}`)
             .then((response) => response.json())
             .then((json) => setPosts(json));
     }, [activeTap]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            window.scrollY >= 200 ? setBackTop(true) : setBackTop(false);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        // cleanUp function
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <div>
@@ -20,11 +33,12 @@ function Content() {
                     {item}
                 </button>
             ))}
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+
+            {backTop && <button style={{ position: 'fixed', bottom: 20, right: 20 }}>Back to Top</button>}
 
             <ul>
                 {posts.map((item, index) => (
-                    <li key={index.id}>{item.title || item.name} </li>
+                    <li key={index}>{item.title || item.name} </li>
                 ))}
             </ul>
         </div>
