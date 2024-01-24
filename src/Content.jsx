f30867f552c4ef6ea15100fd01d078788fc5c449
huynqbg <nqhuysmbg@gmail.com>
2024-01-24 16:11:42 +0700

    useRef() hook

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,22 +1,33 @@
-import { useEffect, useState, useLayoutEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function Content() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(90);
 
-    useLayoutEffect(() => {
-        if (count > 3) setCount(0);
+    const timer = useRef();
+    const prevCount = useRef();
+
+    // render UI thi moi goi call back
+    useEffect(() => {
+        prevCount.current = count;
     }, [count]);
+
+    const handleStart = () => {
+        timer.current = setInterval(() => {
+            setCount((prev) => prev - 1);
+        }, 1000);
+    };
+
+    const handleStop = () => {
+        clearInterval(timer.current);
+    };
+
+    console.log(count, prevCount.current);
 
     return (
         <div style={{ marginTop: 20 }}>
             <h1>{count}</h1>
-            <button
-                onClick={() => {
-                    setCount(count + 1);
-                }}
-            >
-                click
-            </button>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
         </div>
     );
 }
