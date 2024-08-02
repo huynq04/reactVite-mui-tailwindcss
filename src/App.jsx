e112dfd67131ffd69c6328070375a3d0a74a6dbf
huynq04 <huynq.b22cn391@stu.ptit.edu.vn>
2024-08-02 23:53:27 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,71 @@
-import { useEffect } from 'react'
-import { useStore } from './store'
-import { actions } from './store'
+import { Button, TextField, Divider } from '@mui/material'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import Slider from '@mui/material/Slider'
 
-import './App.css'
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein }
+}
 
-function App() {
-    const [state, dispatch] = useStore()
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+]
 
-    const { todoList, todoInput } = state
-
-    const handleAddTodo = () => {
-        dispatch(actions.addTodo(todoInput))
-    }
-
-    useEffect(() => {
-        console.log(todoList)
-    }, [todoList])
-
+export default function App() {
     return (
         <div>
-            <input
-                value={todoInput}
-                placeholder="Enter todo..."
-                onChange={(e) => {
-                    dispatch(actions.setTodoInput(e.target.value))
-                }}
-            />
-
-            <button onClick={handleAddTodo}>Add</button>
-
-            <ul>
-                {todoList.map((todo, index) => (
-                    <li key={index}>{todo}</li>
-                ))}
-            </ul>
+            <div>
+                <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
+                <Button variant="contained" className="bg-black">
+                    Hello world!
+                </Button>
+                <h2>abc</h2>
+            </div>
+            <Divider />
+            <div>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+            </div>
+            <div>
+                <Slider defaultValue={30} />
+                <Slider defaultValue={30} className="text-teal-600" />
+            </div>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Dessert (100g serving)</TableCell>
+                            <TableCell align="right">Calories</TableCell>
+                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.calories}</TableCell>
+                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">{row.carbs}</TableCell>
+                                <TableCell align="right">{row.protein}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     )
 }
-
-export default App
--- src/store/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as ProviderStore } from './Provider'
-export { default as ContextStore } from './Context'
-export * from './hooks'
-
-export * as actions from './actions'
